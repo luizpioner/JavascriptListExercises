845e97927f0cc2ce925c13f6ae2a07ac171e481f
luizpioner <caiopioner@yahoo.com.br>
2026-09-10 17:59:23 -0300

    funcao potencia

--- JavascriptExercise1.js
+++ JavascriptExercise1.js
@@ -3,4 +3,5 @@ function calcularOperacoes(valor1, valor2){
 	console.log("Subtracao:",valor1 - valor2);
 	console.log("Multiplicaco:",valor1 * valor2);
 	console.log("Divisao:", valor1 / valor2);
+	console.log("Potencia:",Math.pow(valor1,valor2));
 }
